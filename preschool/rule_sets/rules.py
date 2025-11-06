13b9a69c2c2d3441c6b928f675772c2edb73cf90
LisaDargasz <234437466+LDarg@users.noreply.github.com>
2025-11-06 18:47:03 +0100

    type goal or constraint for rules added

--- preschool/rule_sets/rules.py
+++ preschool/rule_sets/rules.py
@@ -10,24 +10,25 @@ def set_rules(agent):
     agent.reasoning_unit.reason_theory.add_node('Children have finished their work early and are now running around wildly'.replace(" ", "_"), type='morally relevant fact')
 
     # add obligation-nodes
-    agent.reasoning_unit.reason_theory.add_node('Comfort the child'.replace(" ", "_"), type='moral obligation')
-    agent.reasoning_unit.reason_theory.add_node('Help the child stand up'.replace(" ", "_"), type='moral obligation')
-    agent.reasoning_unit.reason_theory.add_node('Doctor the child'.replace(" ", "_"), type='moral obligation')
-    agent.reasoning_unit.reason_theory.add_node('Stay out of the zone'.replace(" ", "_"), type='moral obligation')
+    agent.reasoning_unit.reason_theory.add_node('Comfort the child'.replace(" ", "_"), type='moral obligation', subclass='goal')
+    agent.reasoning_unit.reason_theory.add_node('Help the child stand up'.replace(" ", "_"), type='moral obligation', subclass='goal')
+    agent.reasoning_unit.reason_theory.add_node('Doctor the child'.replace(" ", "_"), type='moral obligation', subclass='goal')
+    agent.reasoning_unit.reason_theory.add_node('Stay out of the zone'.replace(" ", "_"), type='moral obligation', subclass='constraint')
     
     # add default rules with a hard-coded order among them
     agent.reasoning_unit.reason_theory.add_edge('A child is crying and needs comfort'.replace(" ", "_"), 'Comfort the child'.replace(" ", "_"), 
-                                                    lower_order=set(), 
+                                                    lower_order=set(), prio_trade_off=set(),
                                                     name=f"δ{get_edge_number_as_index(agent)}")
     agent.reasoning_unit.reason_theory.add_edge('A child has fallen down and needs help to get up'.replace(" ", "_"), 'Help the child stand up'.replace(" ", "_"), 
-                                                    lower_order=(('Children have finished their work early and are now running around wildly'.replace(" ", "_"), 'Stay out of the zone'.replace(" ", "_")),), 
+                                                    lower_order=set(), prio_trade_off=set(), 
                                                     name=f"δ{get_edge_number_as_index(agent)}")
     agent.reasoning_unit.reason_theory.add_edge('A child has scratched its knee and needs band-aids'.replace(" ", "_"), 'Doctor the child'.replace(" ", "_"), 
-                                                    lower_order=set(), 
+                                                    lower_order=set(), prio_trade_off=('A child is crying and needs comfort'.replace(" ", "_"), 'Comfort the child'.replace(" ", "_"), 
+                                                                                       'A child has fallen down and needs help to get up'.replace(" ", "_"), 'Help the child stand up'.replace(" ", "_")),
                                                     name=f"δ{get_edge_number_as_index(agent)}")
     agent.reasoning_unit.reason_theory.add_edge('Children have finished their work early and are now running around wildly'.replace(" ", "_"), 'Stay out of the zone'.replace(" ", "_"), 
                                                     lower_order=(('A child is crying and needs comfort'.replace(" ", "_"), 'Comfort the child'.replace(" ", "_")),
-                                                                 ('A child has scratched its knee and needs band-aids'.replace(" ", "_"), 'Doctor the child'.replace(" ", "_"))),
+                                                                 ('A child has fallen down and needs help to get up'.replace(" ", "_"), 'Help the child stand up'.replace(" ", "_"))),
                                                     name=f"δ{get_edge_number_as_index(agent)}")
 
 def get_edge_number_as_index(agent):
